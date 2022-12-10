f8dd23e458034b7bdcbada92c8532153f1520f4d
Lina <liuna.lina@gmail.com>
2022-12-10 08:37:49 -0800

    Solutions of leetcode 74: Search a 2D array

--- 74_search_a_2D_Matrix.py
+++ 74_search_a_2D_Matrix.py
@@ -5,3 +5,22 @@
 
 # Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 # Output: true
+
+class Matrix:
+    def search(self, matrix, target):
+        r = []
+        for row in matrix:
+            if row[0] <= target <= row[len(row) - 1]:
+                r = row
+                break
+        for item in r:
+            if target == item:
+                return True;
+        return False
+
+marr =[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+m = Matrix()
+print(m.search(marr, 3))
+print(m.search(marr, 20))
+print(m.search(marr, 22))
