bd10c22efacdabdc74a4b89f1706a0f78fc485e5
Lina <liuna.lina@gmail.com>
2022-12-20 06:04:29 -0800

    Add 1008 solution, part 1: construct a Node first

--- 1008_Construct_Binary_Search_Tree_from_Preorder.py
+++ 1008_Construct_Binary_Search_Tree_from_Preorder.py
@@ -1,2 +1,24 @@
 # Given an array of integer preorder, which represents the preorder traversal of a BST(binary search tree),
 # construct the tree and return it's root.
+
+class Node:
+    data = 0
+    left = None
+    right = None
+
+    def __int__(self, val):
+        self.data = val
+
+
+class BTree:
+    root = None
+
+    def construct_BTree(self, preorder_arr, start, end):
+        root_node = Node(preorder_arr[start])
+        if start == 0:
+            self.root = root_node
+        if end >= len(preorder_arr):
+            return self.root
+        # root_node.left = self.construct_BTree(preorder_arr, start + 1, end)
+        # root_node.right = self.construct_BTree(preorder_arr, )
+
