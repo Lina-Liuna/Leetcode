a49e2065d91dd92707b6cc3cbd79253d770e57e4
Lina <liuna.lina@gmail.com>
2023-01-05 14:44:10 -0800

    Add Solution 104: Maximum depth of binary tree

--- tree/leetcode_104_maximum_depth_of_binary_tree.py
+++ tree/leetcode_104_maximum_depth_of_binary_tree.py
@@ -5,3 +5,21 @@
 #Input: root = [3,9,20,null,null,15,7]
 #Output: 3
 
+import leetcode_105_construct_btree_thr_preorder_inorder
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+btree = leetcode_105_construct_btree_thr_preorder_inorder.binary_tree()
+btree_root = btree.construct_btree(preorder, inorder)
+
+class max_depth_btree:
+    def maximum_depth_binary_tree(self, root):
+        if root is None:
+            return 0
+        left_height = self.maximum_depth_binary_tree(root.left)
+        right_height = self.maximum_depth_binary_tree(root.right)
+        return 1 + max(left_height, right_height)
+
+
+t = max_depth_btree()
+depth = t.maximum_depth_binary_tree(btree_root)
+print(depth)
