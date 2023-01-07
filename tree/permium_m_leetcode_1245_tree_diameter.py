05945155ebbe361675ec3530c26a4aca7e19ca46
Lina <liuna.lina@gmail.com>
2023-01-07 06:20:23 -0800

    Add Problem 1245: tree diameter: comments examples

--- tree/permium_m_leetcode_1245_tree_diameter.py
+++ tree/permium_m_leetcode_1245_tree_diameter.py
@@ -2,12 +2,12 @@
 # The tree is given as an array of edges where edges[i] = [u, v] is bidirectional edge
 # between nodes u and v.
 # Each node has labels in the set {0, 1, ...,edges.length}
-Input: edges = [[0,1],[0,2]]
-Output: 2
-Explanation:
-A longest path of the tree is the path 1 - 0 - 2.
+# Input: edges = [[0,1],[0,2]]
+# Output: 2
+# Explanation:
+# A longest path of the tree is the path 1 - 0 - 2.
 
-Input: edges = [[0,1],[1,2],[2,3],[1,4],[4,5]]
-Output: 4
-Explanation:
-A longest path of the tree is the path 3 - 2 - 1 - 4 - 5.
+# Input: edges = [[0,1],[1,2],[2,3],[1,4],[4,5]]
+# Output: 4
+# Explanation:
+# A longest path of the tree is the path 3 - 2 - 1 - 4 - 5.
