1551ebea53e85ff52e52c620703d1338b57ba163
Lina <liuna.lina@gmail.com>
2023-01-09 17:31:39 -0800

    Add Solution 1245: tree Diameter, print path between any two nodes in graph

--- tree/permium_m_leetcode_1245_tree_diameter.py
+++ tree/permium_m_leetcode_1245_tree_diameter.py
@@ -19,33 +19,50 @@ class tree_diameter:
 
     def __init__(self):
         self.edges = collections.defaultdict(list)
-        self.visited = list()
+        self.visited = set()
         self.diameter = 0
+        self.path = list()
 
     def get_edges(self, edges):
         for [n1, n2] in edges:
             self.edges[n1].append(n2)
             self.edges[n2].append(n1)
 
-    def get_diameter_between_two_nodes(self, edges, n1, n2):
+    """
+    This is a multi-line comment with docstrings
+    def get_diameter_between_two_nodes(self, n1, n2):
         if n1 == n2:
             return True
         print(n1, n2, self.visited)
-        self.visited.append([n1, n2])
-        self.visited.append([n2, n1])
+        if [n1, n2] not in self.visited:
+            self.visited.append([n1, n2])
+            self.visited.append([n2, n1])
         if [n1, n2] in edges or [n2, n1] in edges:
             print('in edge:', n1, n2)
             return True
         else:
             for n in self.edges[n1]:
-                #print(n, self.edges[n1], n2)
                 if [n, n2] not in self.visited:
                     self.visited.append([n, n2])
                     self.visited.append([n2, n])
                     self.diameter = self.diameter + 1
-                    if self.get_diameter_between_two_nodes(edges, n, n2) is True:
+                    if self.get_diameter_between_two_nodes(n, n2) is True:
                         return self.diameter
         return self.diameter
+"""
+    def path_between_two_nodes(self, n1, n2):
+        self.path.append(n1)
+        if n1 == n2:
+            print(self.path)
+            return True
+        self.visited.add(n1)
+
+        for n in self.edges[n1]:
+            if n not in self.visited:
+                self.diameter = self.diameter + 1
+                self.path_between_two_nodes(n, n2)
+
+        del self.path[-1]
 
 
 edges = [[0, 1], [1, 2], [2, 3], [1, 4], [4, 5]]
@@ -53,5 +70,6 @@ td = tree_diameter()
 
 td.get_edges(edges)
 print(td.edges)
-dis = td.get_diameter_between_two_nodes(edges, 5, 3)
+dis = td.path_between_two_nodes(3, 5)
 print(dis)
+
