7cabb2a844fc72af1654a79b7f7d6ec901bf1c1c
Lina <liuna.lina@gmail.com>
2023-01-17 16:33:58 -0800

    Add Solution: 261 graph valid tree

--- tree/premium_m_261_graph_valid_tree.py
+++ tree/premium_m_261_graph_valid_tree.py
@@ -5,3 +5,40 @@
 # Input: n = 5, and edges = [[0,1], [1,2], [2,3], [1,3], [1,4]]
 # Output: false
 
+import collections
+
+
+class graph_valid_tree:
+    edges = collections.defaultdict(list)
+
+    def __init__(self, edges):
+        for u, v in edges:
+            self.edges[u].append(v)
+            self.edges[v].append(u)
+
+    def dfs(self, u, v, visited):
+        if len(visited) != 0:
+            if u == v:
+                return True
+        visited.append(u)
+        for n in self.edges[u]:
+            print(self.edges[u])
+            if n not in visited:
+                self.dfs(n,v,visited)
+
+    def check_circle_in_graph(self):
+        for u in self.edges.keys():
+            print(u)
+            visited = list()
+            if self.dfs(u,u, visited) is True:
+                return False
+
+        return True
+
+
+Input = [[0,1],[1,2],[2,3],[1,4],[4,5]]
+Input2 = [[0,1],[1,2]]
+Input3 = [[0,1],[1,2],[2,3],[1,4],[4,5],[1,3]]
+g = graph_valid_tree(Input)
+print(g.edges)
+print(g.check_circle_in_graph())
